31e80af784db840d748e4fae290346c2d26f4094
firat2352 <firatcelik52@gmail.com>
2022-07-22 16:05:18 +0300

    comment.js file added ,Note Index edited fro entegrating for modal_comment popup

--- Makale.WebProject/Controllers/YorumController.cs
+++ Makale.WebProject/Controllers/YorumController.cs
@@ -1,5 +1,6 @@
 ﻿using Makale.BusinessLayer;
 using Makale.Entities;
+using Makale.WebProject.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -51,6 +52,61 @@ namespace Makale.WebProject.Controllers
             if (_commentManager.Update(comment) > 0)
             {
                 return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Comment comment = _commentManager.Find(x => x.Id == id);
+
+            if (comment == null)
+            {
+                return new HttpNotFoundResult();
+            }
+
+            if (_commentManager.Delete(comment) > 0)
+            {
+                return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+            }
+
+            return Json(new { result = false }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Comment comment, int? noteid)
+        {
+            ModelState.Remove("CreatedOn");
+            ModelState.Remove("ModifiedOn");
+            ModelState.Remove("ModifiedUsername");
+
+            if (ModelState.IsValid)
+            {
+                if (noteid == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+
+                Note note = _noteManager.Find(x => x.Id == noteid);
+
+                if (note == null)
+                {
+                    return new HttpNotFoundResult();
+                }
+
+                comment.Note = note;
+                comment.Owner = CurrentSession.User;
+
+                if (_commentManager.Insert(comment) > 0)
+                {
+                    return Json(new { result = true }, JsonRequestBehavior.AllowGet);
+                }
             }
 
             return Json(new { result = false }, JsonRequestBehavior.AllowGet);
